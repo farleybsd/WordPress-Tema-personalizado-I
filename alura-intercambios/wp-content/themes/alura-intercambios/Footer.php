af0f0ee6e7b0566dc4c3fec24c9e05478de5a057
farleybsd <farley.t.i@hotmail.com>
2020-09-16 12:44:48 -0300

    Rodapé

--- alura-intercambios/wp-content/themes/alura-intercambios/Footer.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/Footer.php
@@ -1,3 +1,9 @@
+<footer>
+    <p class="container-alura">
+        &copy; <?= date("Y") ?> - Todos os  direitos reservardos a Alura Intercâmbios.
+    </p>
+</footer>
+
 </body>
 <?php wp_footer(); ?>
 </html>
