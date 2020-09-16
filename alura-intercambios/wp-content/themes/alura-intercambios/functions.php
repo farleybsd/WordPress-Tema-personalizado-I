9c919d451d6a755166d4290900a3769a713025df
farleybsd <farley.t.i@hotmail.com>
2020-09-16 07:30:30 -0300

    Loop Wordpress

--- alura-intercambios/wp-content/themes/alura-intercambios/functions.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/functions.php
@@ -3,6 +3,7 @@
 function alura_intercambio_adicionando_recurso_ao_tema()
 {
    add_theme_support('custom-logo'); 
+   add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme','alura_intercambio_adicionando_recurso_ao_tema');
--- alura-intercambios/wp-content/themes/alura-intercambios/index.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once 'Header.php';
-?>
-<h1>Olá Mundo.</h1>
-
-<?php
+if(have_posts()):
+  while(have_posts()): the_post(); 
+  the_post_thumbnail();
+  the_title(); 
+  the_content();
+endwhile;
+endif;
 require_once 'Footer.php';
 ?>
