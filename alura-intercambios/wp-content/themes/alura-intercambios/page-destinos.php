23cb8fa50ac3f179d0225bfdb61c99118422a343
farleybsd <farley.t.i@hotmail.com>
2020-09-17 17:04:05 -0300

    Ajustando filtro

--- alura-intercambios/wp-content/themes/alura-intercambios/page-destinos.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/page-destinos.php
@@ -11,8 +11,9 @@ require_once 'header.php';
            $paises= get_terms(array('taxonomy'=> 'paises'));
 
            foreach($paises as $pais): ?>
-            <option value="<?=$pais->name ?>"><?=$pais->name ?></option>
-           <?php endforeach
+           <option value="<?= $pais->name ?>"
+                <?= !empty($_GET['paises']) && $_GET['paises'] === $pais->name ? 'selected' : '' ?>><?= $pais->name ?></option>
+            <?php endforeach;
         
         ?>
     </select>
@@ -20,15 +21,16 @@ require_once 'header.php';
 </form>
 
 <?php
-$paisSelecionado= array(array(
+if (!empty($_GET['paises'])) {
+    $paisSelecionado= array(array(
     'taxonomy'=>'paises',
     'field'=> 'name',
     'terms'=> $_GET['paises']
 ));
-
+}
 $args = array(
     'post_type' => 'destinos',
-     'tax_query' => $paisSelecionado
+     'tax_query' => !empty($_GET['paises']) ? $paisSelecionado : ''
 );
 $query = new WP_Query($args);
 if ($query->have_posts()):
