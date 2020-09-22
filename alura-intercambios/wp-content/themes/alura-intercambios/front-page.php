bb3e47b636e7b0e52c3d5f88569d6693e332d04d
farleybsd <farley.t.i@hotmail.com>
2020-09-22 13:39:35 -0300

    Trazendo imagem banner

--- alura-intercambios/wp-content/themes/alura-intercambios/front-page.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/front-page.php
@@ -1,7 +1,29 @@
 <?php
+$estiloPagina = 'home.css';
 require_once 'Header.php';
 
-?>
-<h1>GALOO</h1>
-<?php
+$args = array(
+    'post_type'=> 'banners',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+
+);
+
+$query = new WP_Query($args);
+
+if($query->have_posts()):
+
+    while($query->have_posts()) : $query->the_post();
+    ?>
+
+    <main>
+        <div class="imagem-banner">
+            <?php the_post_thumbnail() ?>
+        </div>
+    </main>
+
+    <?php
+    endwhile;
+
+endif;
 require_once 'Footer.php';
--- alura-intercambios/wp-content/themes/alura-intercambios/functions.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/functions.php
@@ -78,12 +78,32 @@ add_action('add_meta_boxes','alura_intercambios_registrando_metabox');
 
 function ai_funcao_callback($post)
 {
+
+    $texto_home_1 = get_post_meta($post->ID,'texto_home_1', true);
+    $texto_home_2 = get_post_meta($post->ID,'texto_home_2', true);
     ?>
-    <label for="texto_home_1">Texto1</label>
-    <input type="text" name="texto_home_1" style="width:100%"/>
+    <label for="texto_home_1">Texto 1</label>
+    <input type="text" name="texto_home_1" style="width: 100%" value="<?= $texto_home_1 ?>"/>
     <br>
     <br>
-    <label for="texto_home_2">Texto2</label>
-    <input type="text" name="texto_home_2" style="width:100%"/>
+    <label for="texto_home_2">Texto 2</label>
+    <input type="text" name="texto_home_2" style="width: 100%" value="<?= $texto_home_2 ?>"/>
     <?php
 }
+
+function alura_intercambios_salvando_dados_metabox($post_id)
+{
+    foreach( $_POST as $key=>$value){
+        if($key !== 'texto_home_1' && $key !== 'texto_home_2'){
+            continue;
+        }
+
+        update_post_meta(
+              $post_id,
+            '_'. $key,
+            $_POST[$key]
+        );
+    }
+}
+
+add_action('save_post','alura_intercambios_salvando_dados_metabox');
