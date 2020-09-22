e120d46dd228e2b112ec32982620b170ae5f40c7
farleybsd <farley.t.i@hotmail.com>
2020-09-22 17:44:22 -0300

    Texto dinâmico

--- alura-intercambios/wp-content/themes/alura-intercambios/functions.php
+++ alura-intercambios/wp-content/themes/alura-intercambios/functions.php
@@ -109,11 +109,36 @@ function alura_intercambios_salvando_dados_metabox($post_id)
 add_action('save_post','alura_intercambios_salvando_dados_metabox');
 
 // js
+function pegandoTextosParaBanner()
+{
+    $args = array(
+        'post_type' => 'banners',
+        'post_status'=> 'publish',
+        'posts_per_page'=> 1,
+
+    );
+    $query = new WP_Query($args);
+
+    if($query->have_posts()):
+        while($query->have_posts()) : $query->the_post() ;
+            $texto1 = get_post_meta(get_the_id(),'_texto_home_1',true);
+            $texto2 = get_post_meta(get_the_id(),'_texto_home_2',true);
+            return array(
+                'texto1'=> $texto1,
+                'texto2'=> $texto2
+            );
+        endwhile;
+    endif;
+}
 
 function alura_intercambios_adicionando_scripts(){
+    
+    $textoBanner = pegandoTextosParaBanner();
+    
     if(is_front_page()){
         wp_enqueue_script('typed-js', get_template_directory_uri() . '/js/typed.min.js', array(), false, true);
         wp_enqueue_script('texto-banner-js', get_template_directory_uri() . '/js/texto-banner.js', array('typed-js'), false, true);
+        wp_localize_script('texto-banner-js','data',$textoBanner);
     }
 }
 add_action('wp_enqueue_scripts', 'alura_intercambios_adicionando_scripts');
